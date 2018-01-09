5fd692d31c82f819e2c63e153c7ebd9f5b58e2bc
goldencat-tom <bluebird6922@gmail.com>
2018-01-09 10:38:13 +0300

    add password controller for user

---
+++ app/controllers/users/passwords_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Users
+  class PasswordsController < Devise::PasswordsController
+    # GET /resource/password/new
+    # def new
+    #   super
+    # end
+
+  end
+end
